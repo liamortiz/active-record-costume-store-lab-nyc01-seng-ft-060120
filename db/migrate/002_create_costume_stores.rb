7bbf9d9d5b1ae45903896bafe60834a4e9450f0a
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 15:15:16 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :in_business
+      t.boolean :still_in_business
       t.time :opening_time
       t.time :closing_time
     end
