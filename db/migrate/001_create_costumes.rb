1fc2e8dc2c8e9fd96a48b531cf3d7c60957d3d1a
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 15:06:57 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,7 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.integer :size
+      t.string :size
       t.string :image_url
       t.timestamps
     end
