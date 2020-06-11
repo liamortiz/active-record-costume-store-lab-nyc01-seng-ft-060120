eddf62be2d761ea1d02607a51006ee3bf06597ec
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 15:05:33 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,18 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.time :opening_time
+      t.time :closing_time
+      t.text :description
+    end
+  end
+end
